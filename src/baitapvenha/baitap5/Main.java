e9b9a8234e0d6964d72e3f9e9f68672f2569647e
tronghung0208 <phanhung0208ytna@gmail.com>
2023-10-12 08:19:11 +0700

    SS16_BT

--- src/baitapvenha/baitap5/Main.java
+++ src/baitapvenha/baitap5/Main.java
@@ -34,9 +34,17 @@ public class Main {
                     listArr.add(item);
                 }
             }
+            int index=0;
+            int maxLength=listArr.get(index).length();
+
 
             for (int i = 0; i < listArr.size(); i++) {
+                if (maxLength<listArr.get(i).length()){
+                    maxLength=listArr.get(i).length();
+                    index=i;
+                }
             }
+            System.out.println("Phần tử có độ dài lớn nhất là "+ maxLength+" là phần tử: "+ listArr.get(index));
         } catch (IOException e) {
             System.out.println("lỗi đọc file");
         }
